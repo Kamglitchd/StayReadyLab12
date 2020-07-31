e4a561fe5f1513af67933fac74212c6b7fe61f14
Kamglitchd <skamara2@umd.edu>
2020-07-31 17:47:02 -0400

    Parenthesis checker complete

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,30 @@
 package io.codedifferently;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class OpenCloseChecker {
+    public static boolean openClose(String openChar) {
+        Map<String, String> parenthesis = new HashMap<>();
+        parenthesis.put("(", ")");
+        parenthesis.put("{","}");
+        parenthesis.put("[","]");
+        parenthesis.put("<",">");
+        parenthesis.put("\"","\"");
+        parenthesis.put("'","'");
+        Stack<Character> braceStack = new Stack();
+        for(char c: openChar.toCharArray()) {
+            if (parenthesis.containsKey(c)) {
+                braceStack.push(c);
+            }if(parenthesis.containsValue(parenthesis.get(c))){
+                braceStack.pop();
+            }
+
+        }
+        System.out.println(braceStack);
+        return braceStack.isEmpty();
+    }
+
 }
+
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,28 @@
 package io.codedifferently;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
 public class OpenCloseCheckerTest {
+   // OpenCloseChecker oc = new OpenCloseChecker();
+    @Test
+    public void test1() {
+        String Sentence = "{ <({[['']]})>}";
+       // assertTrue(wc.openClose(Sentence));
+        assertTrue( OpenCloseChecker.openClose(Sentence));
+    }
+    @Test
+    public void test2() {
+        String Sentence = "{}";
+        assertTrue(OpenCloseChecker.openClose(Sentence));
+    }
+//    @Test public void testFileRead() {
+//        wc.readWords(Arrays.asList("Avengers", "Endgame", "Infinity War"));
+//        assertEquals(26, wc.numWords());
+//        assertEquals(9,wc.longestWord().length());
+//    }
 
 }
